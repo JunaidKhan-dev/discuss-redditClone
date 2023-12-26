45528a29215f14585d27825cec8ee89907906fd6
Junaid <info@junaidkhan.dev>
2023-12-26 11:47:39 +0000

    skeleton pages

---
+++ src/app/topics/[slug]/page.tsx
@@ -0,0 +1,9 @@
+function TopicShowPage() {
+  return (
+    <div>
+      <h1>Topic Show Page</h1>
+    </div>
+  )
+}
+
+export default TopicShowPage
---
+++ src/app/topics/[slug]/posts/new/page.tsx
@@ -0,0 +1,9 @@
+function PostCreatePage() {
+  return (
+    <div>
+      <h1>Post Create Post</h1>
+    </div>
+  )
+}
+
+export default PostCreatePage
